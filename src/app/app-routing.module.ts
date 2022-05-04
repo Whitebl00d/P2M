1500b82708361f742b427640909fd902b4f6db5d
wajdi <wajdi404@gmail.com>
2022-05-05 00:07:40 +0100

    fix routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutusComponent } from './components/aboutus/aboutus.component';
+import { SearchComponent } from './components/search/search.component';
 const routes: Routes = [
   {
     path:'aboutus', 
     component:AboutusComponent
-  }
+  },
+  {
+    path:'',
+    component:SearchComponent
+  }  
   
 ];
 
